c1c81e708effac12a336b774acdf4ac4973c2792
Titov Maks <docent.86@mail.ru>
2016-03-26 01:56:24 +0300

    rework FileManager class

--- SSH_Remote_Client.BL/FileManager.cs
+++ SSH_Remote_Client.BL/FileManager.cs
@@ -1,21 +1,39 @@
 ﻿using System.Text;
 using System.IO;
+using Renci.SshNet;
 
 namespace SSH_Remote_Client.BL
 {
-    class FileManager
+    public interface IFileManager
     {
-        private readonly Encoding _defaultencoding = Encoding.GetEncoding(1200);
+        
+    }
 
-        public string GetContent(string filePath)
+    class FileManager: IFileManager
+    {
+        private readonly string _remotePath = "/tmp/upload";
+
+        public void UploadFile(string userName, string passwd, string host)
         {
-            return GetContent(filePath, _defaultencoding);
+            SftpClient client = new SftpClient(SetConnection(userName, passwd, host));
+            string file = "script.sh";
+            var fileStream = File.OpenRead(file);
+            client.UploadFile(fileStream, _remotePath + file, true);
+            client.Disconnect();
+            client.Dispose();
         }
 
-        public string GetContent(string filePath, Encoding encoding)
+        public string GetFileContent(string filePath)
         {
-            string content = File.ReadAllText(filePath, encoding);
-            return content;
+
+            return null;
+        }
+
+        private ConnectionInfo SetConnection(string username, string passwd, string host, int port = 22)
+        {
+            ConnectionInfo connection = new ConnectionInfo(host, port, username, new AuthenticationMethod[] 
+            { new PasswordAuthenticationMethod(username, passwd) });
+            return connection;
         }
     }
 }
