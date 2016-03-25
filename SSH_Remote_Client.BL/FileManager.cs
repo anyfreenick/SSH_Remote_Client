fdd6d60f597543274b20f42d08d8f2da7a87dbac
Titov Maks <docent.86@mail.ru>
2016-03-26 00:30:45 +0300

    added FileManager class

---
+++ SSH_Remote_Client.BL/FileManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.IO;
+
+namespace SSH_Remote_Client.BL
+{
+    class FileManager
+    {
+        private readonly Encoding _defaultencoding = Encoding.GetEncoding(1200);
+
+        public string GetContent(string filePath)
+        {
+            return GetContent(filePath, _defaultencoding);
+        }
+
+        public string GetContent(string filePath, Encoding encoding)
+        {
+            string content = File.ReadAllText(filePath, encoding);
+            return content;
+        }
+    }
+}
