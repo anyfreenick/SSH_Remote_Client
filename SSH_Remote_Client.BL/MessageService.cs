812f321c49d959ca2078f40001e1ade02ed9079f
Titov Maks <docent.86@mail.ru>
2016-03-25 21:49:35 +0300

    1

---
+++ SSH_Remote_Client.BL/MessageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSH_Remote_Client.BL
+{
+    class MessageService
+    {
+
+    }
+}
