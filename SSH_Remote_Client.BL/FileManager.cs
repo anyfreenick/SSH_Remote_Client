11b3ebe37dea8bda1bb980ffa26d32ee69e09a76
Titov Maks <docent.86@mail.ru>
2016-03-26 08:57:35 +0300

    added constructor with ConnectionInfo

--- SSH_Remote_Client.BL/FileManager.cs
+++ SSH_Remote_Client.BL/FileManager.cs
@@ -14,12 +14,18 @@ namespace SSH_Remote_Client.BL
     public class FileManager: IFileManager
     {
         private readonly string _remotePath = "/tmp/upload";
+        private ConnectionInfo connection;
+
+        public FileManager(string[] authParams)
+        {
+            SetConnection(authParams);
+        }
 
         //убрать хардкодный путь
         public List<string> getFileList(string user, string pass, string host)
         {
             List<string> files = new List<string>();
-            SftpClient sftp = new SftpClient(SetConnection(user, pass, host));
+            SftpClient sftp = new SftpClient(connection);
             sftp.Connect();
             sftp.ChangeDirectory("/home/docent");
             List<SftpFile> filelist = sftp.ListDirectory("/home/docent").ToList();
@@ -46,6 +52,14 @@ namespace SSH_Remote_Client.BL
         {
 
             return null;
+        }
+
+        private void SetConnection(string[] authParams)
+        {
+            connection = new ConnectionInfo(authParams[0]/*host*/, 22, authParams[1]/*username*/, new AuthenticationMethod[]
+            {
+                new PasswordAuthenticationMethod(authParams[1], authParams[2]/*password*/)
+            });
         }
 
         private ConnectionInfo SetConnection(string username, string passwd, string host, int port = 22)
