5b78c4fbd11018130293a7235c891188b75609f1
Titov Maks <docent.86@mail.ru>
2016-03-26 09:49:36 +0300

    refactoring

--- SSH_Remote_Client.BL/FileManager.cs
+++ SSH_Remote_Client.BL/FileManager.cs
@@ -8,29 +8,37 @@ namespace SSH_Remote_Client.BL
 {
     public interface IFileManager
     {
-        
+        string RemotePath { get; set; }
+        List<string> GetFileList();
+        void UploadFile(string filePath);
     }
 
     public class FileManager: IFileManager
     {
-        private readonly string _remotePath = "/tmp/upload";
-        private ConnectionInfo connection;
+        private string _remotePath;
+        private ConnectionInfo _connection;
 
-        public FileManager(string[] authParams)
+        public string RemotePath
         {
+            get { return _remotePath; }
+            set { _remotePath = value; }
+        }
+
+        public FileManager(string host, string username, string pass)
+        {
+            string[] authParams = { host, username, pass };
             SetConnection(authParams);
         }
 
-        //убрать хардкодный путь
-        public List<string> getFileList()
+        public List<string> GetFileList()
         {
             List<string> files = new List<string>();
-            SftpClient sftp = new SftpClient(connection);
+            SftpClient sftp = new SftpClient(_connection);
             sftp.Connect();
-            sftp.ChangeDirectory("/home/docent");
-            List<SftpFile> filelist = sftp.ListDirectory("/home/docent").ToList();
+            List<SftpFile> filelist = sftp.ListDirectory(_remotePath).ToList();
             foreach (SftpFile file in filelist)
             {
+                if (!file.IsDirectory)
                 files.Add(file.Name);
             }
             sftp.Disconnect();
@@ -38,27 +46,25 @@ namespace SSH_Remote_Client.BL
             return files;
         }
 
-        public void UploadFile(string userName, string passwd, string host)
+        public void UploadFile(string filePath)
         {
-            SftpClient client = new SftpClient(connection);
-            string file = "script.sh";
-            var fileStream = File.OpenRead(file);
-            client.UploadFile(fileStream, _remotePath + file, true);
+            SftpClient client = new SftpClient(_connection);
+            var fileStream = File.OpenRead(filePath);
+            client.UploadFile(fileStream, _remotePath + fileStream.Name, true);
             client.Disconnect();
             client.Dispose();
         }
 
-        public string GetFileContent(string filePath)
+        public void SetRemotePath(string path)
         {
-
-            return null;
+            _remotePath = path;
         }
 
         private void SetConnection(string[] authParams)
         {
-            connection = new ConnectionInfo(authParams[0]/*host*/, 22, authParams[1]/*username*/, new AuthenticationMethod[]
+            _connection = new ConnectionInfo(authParams[0]/*host*/, 22, authParams[1]/*username*/, new AuthenticationMethod[]
             {
-                new PasswordAuthenticationMethod(authParams[1], authParams[2]/*password*/)
+                new PasswordAuthenticationMethod(authParams[1]/*username*/, authParams[2]/*password*/)
             });
         }        
     }
