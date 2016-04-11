4eec9225aa8c4e829ac37c42365c8e55e028bc15
anyfreenick <titov.maks86@gmail.com>
2016-04-11 18:36:21 +0300

    refactoring

--- SSH_Remote_Client.BL/SettingsManager.cs
+++ SSH_Remote_Client.BL/SettingsManager.cs
@@ -1,5 +1,4 @@
 ﻿using IniParser;
-using IniParser.Model;
 using System.Collections.Generic;
 using System.Text;
 
@@ -25,9 +24,9 @@ namespace SSH_Remote_Client.BL
         public List<string> LoadSections(string filePath)
         {
             var parser = new FileIniDataParser();
-            IniData parsedData = parser.ReadFile(filePath);
+            var parsedData = parser.ReadFile(filePath);
             List<string> result = new List<string>();
-            foreach (SectionData section in parsedData.Sections)
+            foreach (var section in parsedData.Sections)
                 result.Add(section.SectionName);
             return result;
         }
@@ -42,7 +41,7 @@ namespace SSH_Remote_Client.BL
         {
             string[] result = new string[4];
             var parser = new FileIniDataParser();
-            IniData parsedData = parser.ReadFile(filePath);
+            var parsedData = parser.ReadFile(filePath);
             result[0] = parsedData[sectionName]["username"];
             result[1] = parsedData[sectionName]["password"];
             result[2] = parsedData[sectionName]["host"];
@@ -62,7 +61,7 @@ namespace SSH_Remote_Client.BL
         public void AddSectionToConfig(string sectionName, string username, string password, string host, string remotePath, string configFile)
         {
             var parser = new FileIniDataParser();
-            IniData parsedData = parser.ReadFile(configFile);
+            var parsedData = parser.ReadFile(configFile);
             parsedData.Sections.AddSection(sectionName);
             parsedData[sectionName].AddKey("username", username);
             parsedData[sectionName].AddKey("password", password);
@@ -79,7 +78,7 @@ namespace SSH_Remote_Client.BL
         public void RemoveSectionFromConfig(string sectionName, string filePath)
         {
             var parser = new FileIniDataParser();
-            IniData data = parser.ReadFile(filePath);
+            var data = parser.ReadFile(filePath);
             data.Sections.RemoveSection(sectionName);
             parser.WriteFile(filePath, data, Encoding.UTF8);
         }
